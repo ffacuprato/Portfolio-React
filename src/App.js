8e1e02a3da7d8d06ab6926fc88a30758ed324379
Facundo <facundoprato@gmail.com>
2023-11-07 19:40:24 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my portfolio <code>src/App.js</code> and save to reload.
+          Esta es mi aplicacion
         </p>
         <a
           className="App-link"
